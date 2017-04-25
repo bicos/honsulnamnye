f66418a087988af5dd013d1fb8fc29c33686c607
bicos <bicos@naver.com>
2017-04-25 09:12:28 +0900

    Initial commit

---
+++ app/src/main/java/com/obppamanse/honsulnamnye/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.obppamanse.honsulnamnye;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ravy on 2017. 4. 15..
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
