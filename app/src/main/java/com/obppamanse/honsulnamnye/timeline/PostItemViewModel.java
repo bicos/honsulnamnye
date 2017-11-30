3f42555c83cd162cb6317b2b2d34dab371b88221
raehyeong.park <raehyeong.park@rgpkorea.co.kr>
2017-11-30 21:36:23 +0900

    1. 리스트 아이템 변경

--- app/src/main/java/com/obppamanse/honsulnamnye/timeline/PostItemViewModel.java
+++ app/src/main/java/com/obppamanse/honsulnamnye/timeline/PostItemViewModel.java
@@ -2,10 +2,15 @@ package com.obppamanse.honsulnamnye.timeline;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.text.TextUtils;
 import android.view.View;
 
 import com.obppamanse.honsulnamnye.post.detail.PostDetailActivity;
+import com.obppamanse.honsulnamnye.post.model.Participant;
 import com.obppamanse.honsulnamnye.post.model.Post;
+import com.obppamanse.honsulnamnye.util.DateUtils;
+
+import java.util.Map;
 
 /**
  * Created by Ravy on 2017. 6. 11..
@@ -31,6 +36,28 @@ public class PostItemViewModel extends BaseObservable {
     @Bindable
     public String getDesc() {
         return post != null ? post.getDesc() : null;
+    }
+
+    @Bindable
+    public String getDate() {
+        return post != null && post.getDueDateTime() != 0L ? DateUtils.getDateStr(post.getDueDateTime()) : "날짜 미정";
+    }
+
+    @Bindable
+    public String getPlaceName() {
+        return post != null && post.getPlace() != null && !TextUtils.isEmpty(post.getPlace().getName()) ?
+                post.getPlace().getName() :
+                "장소 미정";
+    }
+
+    @Bindable
+    public String getParticipantsDesc(){
+        if (post == null || post.getParticipantList() == null) {
+            return "0명 참여중";
+        }
+
+        Map<String, Participant> participantMap = post.getParticipantList();
+        return participantMap.keySet().size() + "명 참여중";
     }
 
     public void clickListItem(View view) {
